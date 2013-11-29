d029a2eabf1cfeff8f6ad3966213ccf660cf76ac
Ivan <ivan__wissler@hotmail.com>
2013-11-29 14:00:08 +0100

    ViewClasses

---
+++ src/main/java/ch/bfh/btx8081/w2013/red/GUI/SearchDis.java
@@ -0,0 +1,37 @@
+package ch.bfh.btx8081.w2013.red.GUI;
+
+import ch.bfh.btx8081.w2013.red.Controller.NavigatorUI;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
+
+public class SearchDis extends VerticalLayout implements View {
+	final VerticalLayout layout;
+	public SearchDis()
+	{
+		this.layout = this;
+		createLayout();
+	}
+	private void createLayout()
+	{
+		layout.setMargin(true);
+		layout.addComponent(new Label("SearchDis"));
+		Button button = new Button("Click Me");
+        button.addClickListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                layout.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        layout.addComponent(button);
+	}
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
