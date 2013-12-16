93a061eb900b100c9fd2da26b3064fa0870161c0
Swetlana <sians1@fbh.ch>
2013-12-16 19:55:20 +0100

    SearchMed with comment

--- src/main/java/ch/bfh/btx8081/w2013/red/GUI/SearchMed.java
+++ src/main/java/ch/bfh/btx8081/w2013/red/GUI/SearchMed.java
@@ -1,6 +1,11 @@
 /**
+ * The SearchMedi class creates a view in which a user can select Information
+ * about Medication and navigate through the application. 
+ * The Buttons "Search" navigate to class "InfoMed" and Buttons "Main" 
+ * navigate to class "Home"
  * 
  * @author sians1
+ * @version 16.12.2013
  * 
  * */
 
@@ -21,97 +26,128 @@ import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.themes.ChameleonTheme;
 
+@SuppressWarnings("serial")
+
 public class SearchMed extends VerticalLayout implements View, IState {
-	
+
 	final VerticalLayout layout;
 	private AbsoluteLayout mainLayout;
 	private VerticalLayout upperVerticalLayout;
 	private HorizontalLayout lowerHorizontalLayout;
 	private ComboBox comboBox_MediName;
 	private ComboBox comboBox_MediArt;
-	private Label label_MediName;
-	private Label label_MediArt;
 	MhcGuidDesign design;
-	
-        public SearchMed()
-        {
-        	design = new MhcGuidDesign(this);
-    		this.layout = design.getLayout();
-    		this.mainLayout = design.getMainLayout();
-    		this.upperVerticalLayout = design.getUpperVerticalLayout();
-    		this.lowerHorizontalLayout = design.getLowerHorizontalLayout();
-    		editMainLayout();
-    		editUpperVerticalLayout();
-    		editLowerHorizontalLayout();
-        }
-        
-        private void editUpperVerticalLayout()
-    	{
-    		design.setTitleLabel("Medication");
-    	}
-   
-       
-    	private void editMainLayout()
-    	{		
-    		label_MediName = new Label();
-    		label_MediName.setValue("Medication / Active substance");
-    		mainLayout.addComponent(label_MediName, "top:100.0px;left:30.0px;");
-    		
-    		comboBox_MediName = new ComboBox();
-    		mainLayout.addComponent(comboBox_MediName, "top:120.0px;left:30.0px;");
-    		
-    		label_MediArt = new Label();
-    		label_MediArt.setValue("Type of medication ");
-    		mainLayout.addComponent(label_MediArt, "top:160.0px;left:30.0px;");
-    		
-    		comboBox_MediArt= new ComboBox();
-    		mainLayout.addComponent(comboBox_MediArt, "top:180.0px;left:30.0px;");
-    	
-    	}
-        
-        private void editLowerHorizontalLayout()
-        {
-                Button searchButton = new Button("Search");
-                searchButton.setWidth("80px");
-                searchButton.addClickListener(new Button.ClickListener()
-                {
-            public void buttonClick(ClickEvent event) {
-                    handleB2();
-            }
-        });
-                
-                Button mainButton = new Button("Main");
-                mainButton.setWidth("80px");
-                mainButton.addClickListener(new Button.ClickListener()
-                {
-            public void buttonClick(ClickEvent event) {
-                    handleB1();
-            }
-        });
-                
-                lowerHorizontalLayout.addComponent(searchButton);
-                lowerHorizontalLayout.setComponentAlignment(searchButton, Alignment.MIDDLE_LEFT);
-                lowerHorizontalLayout.addComponent(mainButton);
-                lowerHorizontalLayout.setComponentAlignment(mainButton, Alignment.MIDDLE_RIGHT);
-        }
-        @Override
-        public void enter(ViewChangeEvent event) {
-        
-                // TODO Auto-generated method stub
-                
-        }
-        @Override
-        public void handleB1() {
-                NavigatorUI.navigateTo(NavigatorUI.HOMEVIEW);
-        }
-        @Override
-        public void handleB2() {
-                NavigatorUI.navigateTo(NavigatorUI.INFOMEDVIEW);        
-        }
-        @Override
-        public void handleB3() {
-                // TODO Auto-generated method stub
-                
-        }
+
+	/**
+	 * Constructs a SearchMed on the base of different parameters.
+	 *  @param design
+	 *   basic design of the view based on the class MhcGuidDesign
+	 * @param layout
+	 *   basic layout of the view
+	 * @param mainLayout
+	 *  main part of the layout
+	 * @param upperVerticalLayout
+	 *  upper part of the layout
+	 * @param lowerHorizontalLayout
+	 *   lower part of the layout
+	 */
+
+	public SearchMed()
+	{
+		design = new MhcGuidDesign(this);
+		this.layout = design.getLayout();
+		this.mainLayout = design.getMainLayout();
+		this.upperVerticalLayout = design.getUpperVerticalLayout();
+		this.lowerHorizontalLayout = design.getLowerHorizontalLayout();
+		editMainLayout();
+		editUpperVerticalLayout();
+		editLowerHorizontalLayout();
+	}
+
+	/**
+	 * Edits the upperVerticalLayout by setting the TitleLabel.
+	 */
+
+	private void editUpperVerticalLayout()
+	{
+		design.setTitleLabel("Medication");
+	}
+
+	/**
+	 * Here are elements to select. Two combobox for Medication selection  of name (or active substance)
+	 *  and of type
+	 * 
+	 * */     
+	private void editMainLayout()
+	{		
+
+		comboBox_MediName = new ComboBox("Medication / Active substance");
+		mainLayout.addComponent(comboBox_MediName, "top:120.0px;left:30.0px;");
+
+		comboBox_MediArt= new ComboBox("Type of medication ");
+		mainLayout.addComponent(comboBox_MediArt, "top:170.0px;left:30.0px;");
+
+	}
+
+	/**
+	 * Edits the lowerHorizontalLayout by search a button to navigate to class 
+	 * InfoMed and a button "Main" to navigate to the HOMEVIEW.
+	 * 
+	 * */
+
+	private void editLowerHorizontalLayout()
+	{
+		Button searchButton = new Button("Search");
+		searchButton.setWidth("80px");
+		searchButton.addClickListener(new Button.ClickListener()
+		{
+			public void buttonClick(ClickEvent event) {
+				handleB2();
+			}
+		});
+
+		Button mainButton = new Button("Main");
+		mainButton.setWidth("80px");
+		mainButton.addClickListener(new Button.ClickListener()
+		{
+			public void buttonClick(ClickEvent event) {
+				handleB1();
+			}
+		});
+
+		lowerHorizontalLayout.addComponent(searchButton);
+		lowerHorizontalLayout.setComponentAlignment(searchButton, Alignment.MIDDLE_LEFT);
+		lowerHorizontalLayout.addComponent(mainButton);
+		lowerHorizontalLayout.setComponentAlignment(mainButton, Alignment.MIDDLE_RIGHT);
+	}
+
+	/**
+	 * Not overridden method of the interface view.
+	 */
+	public void enter(ViewChangeEvent event) {
+
+	}
+
+	/**
+	 * Navigates back the the last view using methods of the NavigatorUI class.
+	 */
+
+	public void handleB1() {
+		NavigatorUI.navigateTo(NavigatorUI.HOMEVIEW);
+	}
+
+	/**
+	 *  Navigates back to the HOMEVIEW using methods of the NavigatorUI class.
+	 */
+	public void handleB2() {
+		NavigatorUI.navigateTo(NavigatorUI.INFOMEDVIEW);        
+	}
+
+	/**
+	 * Not overridden method of the interface IState.
+	 */
+	public void handleB3() {
+
+	}
 
 }
